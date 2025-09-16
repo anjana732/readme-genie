7ad37f19cf83c4106d2090303ee275d6f1507253
assignment <prakitz2908@gmail.com>
2025-09-16 23:54:12 +0530

    Added Mocker Logic to Send and Verify OTP of Registered User

--- apps/backend/user/user_routes.py
+++ apps/backend/user/user_routes.py
@@ -1,26 +1,36 @@
+import copy
+import re
+
 from fastapi import APIRouter
-from pydantic import BaseModel
-from user.user_login_main import UserLogin
+from pydantic import BaseModel, field_validator
+
+from user.user_login_main import UserMobileLogin
 
 
-class UserLoginModel(BaseModel):
-    username: str
-    password: str
+class MobileLoginModel(BaseModel):
+    mobile: str
+
+    @field_validator("mobile")
+    def validate_mobile(cls, v: str) -> str:
+        if not re.fullmatch(r"^[6-9]\d{9}$", v):
+            raise ValueError("Invalid mobile number format")
+        return v
 
 
 user_router = APIRouter(
-    prefix="/login",
+    prefix="/auth",
     tags=["user operations"]
 )
 
 
-@user_router.post("/")
-def user_login(body: UserLoginModel):
+@user_router.post("/phone")
+def user_login(body: MobileLoginModel):
     func_status = True
+    data = copy.deepcopy(body.model_dump())
     is_success = False
     message = None
     try:
-        user_login_obj = UserLogin(body)
+        user_login_obj = UserMobileLogin(data)
         func_status, is_success, message = user_login_obj.user_login_main_func()
         if not func_status:
             raise Exception("Error in user login, Please try again !!")
